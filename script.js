f357ca68a08a8d066e0bde1cb9d1f5e20f74b004
André <andrerodartematos@gmail.com>
2026-05-10 00:33:52 -0300

    Tutorial completo. Só falta estilizarl.

---
+++ script.js
@@ -0,0 +1,13 @@
+const formNewsletter = document.getElementById("form-newsletter");
+const emailInput = document.getElementById("email-input");
+
+formNewsletter.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const email = emailInput.value;
+
+  alert(
+    "Obrigado por se inscrever, senhor veríssimo! \nNovidades serão enviadas para: ${email}",
+  );
+  emailInput.value = "";
+});
