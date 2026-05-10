d36ea68ce6cb90185bb38d8b053396deb7d6eba8
André <andrerodartematos@gmail.com>
2026-05-10 17:43:50 -0300

    Adicionado o Player de Música

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ formNewsletter.addEventListener("submit", function (event) {
   );
   emailInput.value = "";
 });
+
+const music = document.getElementById("bg-music");
+const playBtn = document.getElementById("play-pause");
+const volumeSlider = document.getElementById("volume-slider");
+
+// Play / Pause
+playBtn.addEventListener("click", () => {
+  if (music.paused) {
+    music.play();
+    playBtn.innerHTML = '<i class="bi bi-pause-fill"></i> Pause';
+  } else {
+    music.pause();
+    playBtn.innerHTML = '<i class="bi bi-play-fill"></i> Play';
+  }
+});
+
+// Mixer de Volume
+volumeSlider.addEventListener("input", (event) => {
+  // O valor do input (0 a 1) altera diretamente o volume do áudio
+  music.volume = event.target.value;
+});
